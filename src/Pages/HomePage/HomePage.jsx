1f8db87bbb1a8bf800c10a2966397459760c5970
Javier Sanchez <javi.sm.77@gmail.com>
2023-10-19 20:56:37 +0200

    arreglo fuente

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -8,7 +8,8 @@ const HomePage = () => {
                 <img className='h__spain' src='../../../assets/spain-1.png' alt='spain' />
                 <img className='h__uk' src='../../../assets/united-kingdom-1.png' alt='united kingdom' />
             </div>
-            <img className='h__img' src='../../../assets/Game-of-Thrones-Logo.png' alt='Game of thrones' />
+            <h1 className='h__game'>GAME OF THRONES</h1>
+            {/* <img className='h__img' src='../../../assets/Game-of-Thrones-Logo.png' alt='Game of thrones' /> */}
             <div className='h__footer'>
                 {/* <Link className='h__home' to="/">Home</Link> */}
                 <Link className='h__characters' to="/characters">Characters</Link>
