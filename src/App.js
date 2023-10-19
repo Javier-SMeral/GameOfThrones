25d54b89bd6c8b5e7700a12db2b2e7ed347bcacc
Javier Sanchez <javi.sm.77@gmail.com>
2023-10-19 16:03:58 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/main/index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body className='c__body'>
+      <header className='c__header'>
+
       </header>
-    </div>
+      <main className='c__main'>
+
+      </main>
+      <footer className='c__footer'>
+
+      </footer>
+    </body>
   );
 }
 
