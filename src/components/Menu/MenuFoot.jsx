6c17f65f6449cdf2b60fda07e165b03b10854441
Javier Sanchez <javi.sm.77@gmail.com>
2023-10-21 13:21:53 +0200

    añado home button

--- src/components/Menu/MenuFoot.jsx
+++ src/components/Menu/MenuFoot.jsx
@@ -6,7 +6,6 @@ const MenuFoot = ({ t }) => {
 
     return (
         <div className='h__footer'>
-            {/* <Link className='h__home' to="/">{t(Home)}</Link> */}
             <Link className='h__characters' to="/characters">{t('characters')}</Link>
             <Link className='h__houses' to="/houses">{t('houses')}</Link>
             <Link className='h__chronology' to="/chronology">{t('chronology')}</Link>
