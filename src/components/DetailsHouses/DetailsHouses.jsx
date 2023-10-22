1d21ace57fa7e7af212b59df840a91786c989c84
Riky Esteban <145079964+destgar@users.noreply.github.com>
2023-10-22 19:44:36 +0200

    Componente DetailsHousesPage

--- src/components/DetailsHouses/DetailsHouses.jsx
+++ src/components/DetailsHouses/DetailsHouses.jsx
@@ -1,19 +1,96 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import axios from "axios";
+import MenuHead from "../../components/Menu/MenuHead";
 
-export default function DetailsHousePage() {
+export default function DetailsHousesPage() {
     const { id } = useParams();
-    const [character, setHouse] = useState(null);
+    const [house, setHouse] = useState(null); 
 
     useEffect(() => {
         const getHouse = async () => {
             try {
-                const { data } = await axios.get(`http://localhost:3000/characters/${id}`);
-                setHouse(data);
+                const houseResponse = await axios.get(`http://localhost:3000/houses/${id}`);
+                setHouse(houseResponse.data);
+
             } catch (error) {
                 console.error(error);
             }
         };
         getHouse();
     }, [id]);
+
+    if (!house) {
+        return <div>Cargando datos o error en la carga.</div>;
+    }
+
+
+    return (
+
+        <div>
+        <div id='mh__head'>
+            
+            <Link className='h__volver' to="/characters">
+            <img src="/assets/arrowBack.png" alt="arrow" style={{ width: '30px' }}></img>
+            <p>Vuelve</p>
+            </Link>
+            <MenuHead/>
+            </div>
+            <div className="houses">
+                <div className="house">
+
+                    <div className="houses">
+                        <div key={house.id} className="house">
+
+                            <img
+                                className="house-image"
+                                src={`http://localhost:3000${house.image}`}
+                                alt={house.name}
+                            />
+                            <h1 className="house-name">{house.name}</h1>
+                        </div>
+
+                        <div className="house-info">
+                            <div className="house-details">
+
+                                <div className="house__settlement">
+                                    <h3>Settlement: </h3>
+                                    <p>{house.settlement}</p>
+                                </div>
+
+                                <div className="house__region">
+                                    <h3>Region: </h3>
+                                    <p>{house.region}</p>
+                                </div>
+                                
+                                <div className="house__alliances">
+                                    <h3>Alliances: </h3>
+                                    <ul>
+                                        {house.alliances.map((alliance, index) => (
+                                            <li key={index}>{alliance}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                                
+                                <div className="house__religions">
+                                    <h3>Religions: </h3>
+                                    <ul>
+                                        {house.religions.map((religion, index) => (
+                                            <li key={index}>{religion}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+
+                                <div className="house__foundation">
+                                    <h3>Foundation: </h3>
+                                    <p>{house.foundation}</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
