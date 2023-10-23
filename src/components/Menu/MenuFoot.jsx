cd006fed4371a6a190aadf1bc70c860c2f7d2274
Jesus Perez <jesusperez_14@hotmail.com>
2023-10-23 02:01:31 +0200

    Menu Underline

--- src/components/Menu/MenuFoot.jsx
+++ src/components/Menu/MenuFoot.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
+import "./menuFooter.css"
 
 const MenuFoot = ({ t }) => {
+    const location = useLocation();
 
-
+//HE CREADO OTRA CLASE DENTRO DE LINK PARA PODER HACER EL UNDERLINE.
     return (
         <div className='h__footer'>
-            <Link className='h__characters' to="/characters">{t('characters')}</Link>
-            <Link className='h__houses' to="/houses">{t('houses')}</Link>
-            <Link className='h__chronology' to="/chronology">{t('chronology')}</Link>
+            <Link className={`button ${location.pathname === '/characters' ? 'active' : ''}`} to="/characters">{t('characters')}</Link>
+            <Link className={`button ${location.pathname === '/houses' ? 'active' : ''}`} to="/houses">{t('houses')}</Link>
+            <Link className={`button ${location.pathname === '/chronology' ? 'active' : ''}`} to="/chronology">{t('chronology')}</Link>
         </div>
     )
 }
